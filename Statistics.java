4b4729c10c75a2762032052a38e7bd0daffef307
Andy Du-frigstad <47784086+s278552@users.noreply.github.com>
2022-03-11 08:52:39 -0500

    sending to other computer

---
+++ Statistics.java
@@ -0,0 +1,34 @@
+/** calculating and finding the mean, median and mode of 3 text files
+ * 
+ * Author Andrew Du-frigstad
+ * version 1
+ * since   2022-03-010
+ * */ 
+
+//imports
+import java.io.*;
+
+class Statistics {
+
+    public static void main(String[] args) {
+        
+        // try catch for the list
+        try {
+            // converting array to an array of integers
+            arrayInt = Integer.parseInt(array);
+        } catch (NumberFormatException e) {
+            arrayInt = -1;
+        }
+        
+        // printing numbers of the text file and going through all the things in the array looking for errors
+        while (arrayCount =! arrayNum) {
+               
+            // printing error message
+            if (arrayInt == -1) {
+                System.out.println("There is a string in the text file, remove it");
+            } else {
+                System.out.println("");
+            }
+        }
+    }
+}

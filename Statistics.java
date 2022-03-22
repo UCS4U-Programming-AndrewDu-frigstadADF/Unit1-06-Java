fb5d1ce2fde2fcb842f9f768119b9a8935773e7f
Andy Du-frigstad <47784086+s278552@users.noreply.github.com>
2022-03-21 22:35:30 -0400

    Finally done

--- Statistics.java
+++ Statistics.java
@@ -7,76 +7,158 @@
 
 //imports
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 class Statistics {
 
-    public static void main(String[] args) {
+    // calculating mean
+    public static Float calcMean(int[] array) {
 
-        String st;
+        // declaring variables
+        float arrayTotal = 0;
 
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("which text file do you want?");
-        String textFileSelect = myObj.nextLine();
+        // adding all hte numbers in the array together
+        for (int arrayIndex = 0; arrayIndex < array.length; arrayIndex = arrayIndex + 1) {
+            arrayTotal += array[arrayIndex];
+        }
 
-        if (textFileSelect == "1") {
+        // calculating the mean
+        Float mean = (float) (arrayTotal / array.length);
 
-            // reading the text files
-            File file1 = new File(
-                    "C:\\Users\\s278552\\Everything\\Unit1-06.txt\\Unit1-06-Java\\set1.txt");
+        // return the mean
+        return mean;
+    }
 
-            // Creating an object of BufferedReader class
-            BufferedReader br1 = new BufferedReader(new FileReader(file1));
+    public static Float calcMedian(int[] array) {
 
-            while ((st = br1.readLine()) != null) {
-                // Print the string
-                System.out.println(st);
-            }
+        // declare variables
+        float median = 0;
+        float findMedian1 = 0;
+        float findMedian2 = 0;
 
-        } else if (textFileSelect == "2") {
+        // sort the array
+        Arrays.sort(array);
 
-            // reading the text files
-            File file2 = new File(
-                    "C:\\Users\\s278552\\Everything\\Unit1-06.txt\\Unit1-06-Java\\set2.txt");
+        if (array.length % 2 == 0) {
 
-            // Creating an object of BufferedReader class
-            BufferedReader br2 = new BufferedReader(new FileReader(file2));
+            // find the median if the number is even
+            findMedian1 = (float) array[array.length / 2];
+            findMedian2 = (float) array[(array.length / 2) + 1];
 
-            while ((st = br2.readLine()) != null) {
-                // Print the string
-                System.out.println(st);
-            }
-
-        } else if (textFileSelect == "3") {
-
-            // reading the text files
-            File file3 = new File(
-                    "C:\\Users\\s278552\\Everything\\Unit1-06.txt\\Unit1-06-Java\\set3.txt");
-
-            // Creating an object of BufferedReader class
-            BufferedReader br3 = new BufferedReader(new FileReader(file3));
-
-            while ((st = br3.readLine()) != null) {
-                // Print the string
-                System.out.println(st);
-            }
+            median = (findMedian1 + findMedian2) / 2;
 
         } else {
-            System.out.println("That is not a vlid input");
+            // find the median if the number is odd
+            median = (float) array[array.length / 2];
         }
 
-        // declaring string variable
+        return median;
+    }
 
-        // try catch for the list
-        try {
-            // converting array to an array of integers
-            arrayInt = Integer.parseInt(array);
-        } catch (NumberFormatException e) {
-            arrayInt = -1;
+    public static List<Integer> calcMode(int[] array) {
+
+        // declaring variables
+        int counter = 0;
+        int biggestNum = counter;
+        List<Integer> mode = new ArrayList<Integer>();
+
+        // sorting the array
+        Arrays.sort(array);
+
+        // finding the mode
+        for (int arrayIndex = 1; arrayIndex < array.length; arrayIndex = arrayIndex + 1) {
+            if (array[arrayIndex] != array[arrayIndex - 1]) {
+
+                if (counter > biggestNum) {
+
+                    // seeing how many of a number repeats
+                    biggestNum = counter;
+
+                    // remembering the mode
+                    mode.clear();
+                    mode.add(array[arrayIndex - 1]);
+
+                } else if (counter == biggestNum) {
+                    mode.add(array[arrayIndex - 1]);
+                }
+                counter = 1;
+
+            } else {
+
+                counter = counter + 1;
+            }
         }
+        return mode;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // declaring variables
+        String st;
+        ArrayList<String> txtList = new ArrayList<String>();
+        String txtFileSelect = "";
+        int checkingInt = -1;
+
+        // getting the number of text file the user wants to use
+        Scanner myObj = new Scanner(System.in);
+        while (!txtFileSelect.equals("1") && !txtFileSelect.equals("2") && !txtFileSelect.equals("3")) {
+            System.out.println("Which txt file: 1, 2, or 3: ");
+            txtFileSelect = myObj.nextLine();
+        }
+
+        // reading the text file
+        File file = new File("C:\\Users\\andye\\Downloads\\set" + txtFileSelect + ".txt");
+
+        // printing out all the numbers in the text file
+        System.out.println(file);
+        try (Scanner sc = new Scanner(file)) {
+            // makes it into a list kind of
+            while (sc.hasNextLine()) {
+                txtList.add(sc.nextLine());
+            }
+        }
+
+        System.out.println(txtList);
+
+        int[] num = new int[txtList.size()];
+        for (int arrayInt = 0; arrayInt < txtList.size(); arrayInt = arrayInt + 1) {
+
+            // try catch for the list
+            try {
+                // converting list index to an integer
+                checkingInt = Integer.parseInt(txtList.get(arrayInt));
+            } catch (NumberFormatException e) {
+                checkingInt = -1;
+                break;
+            }
+
+            // putting integer into array
+            num[arrayInt] = checkingInt;
+        }
+
         // printing error message
-        if (arrayInt == -1) {
+        if (checkingInt == -1) {
             System.out.println("There is a string in the text file, remove it");
+
+            // allows the user to input a different text file when error occurs
+            while (!txtFileSelect.equals("1") && !txtFileSelect.equals("2") && !txtFileSelect.equals("3")) {
+                System.out.println("Which txt file: 1, 2, or 3: ");
+                txtFileSelect = myObj.nextLine();
+            }
         }
+
+        // print the mean of the text file and call that method
+        float mean = calcMean(num);
+        System.out.println("Your mean is " + mean);
+
+        // print the mean of the text file and call that method
+        float median = calcMedian(num);
+        System.out.println("Your median is " + median);
+
+        List<Integer> mode = calcMode(num);
+        System.out.println("Your median is " + mode);
     }
 }
